d8c56eb671dc01c4dffdaea4955efeb5de0da865
Timothy Purchas <timothy.purchas@iag.com.au>
2020-02-28 16:35:44 +1100

    fix commands

--- packages/@carnivalofthecosmos/pipeline/lib/commands.ts
+++ packages/@carnivalofthecosmos/pipeline/lib/commands.ts
@@ -1,8 +1,11 @@
-export const ECR_LOGIN = '$(aws ecr get-login --no-include-email --region $(AWS_REGION))';
+import { Aws } from '@aws-cdk/core';
+
+export const ECR_LOGIN = `$(aws ecr get-login --no-include-email --region ${Aws.REGION})`;
 
 export const NPM_INSTALL = 'npm ci';
 
 export const NPM_RUN_BUILD = 'npm run build';
 
 // eg export APP_BUILD_VERSION=v1.0.0-1
-export const NPM_EXPORT_APP_BUILD_VERSION = `export APP_BUILD_VERSION=v$(node -p "require('./package.json').version")-$(CODEBUILD_BUILD_NUMBER)`;
+export const NPM_EXPORT_APP_BUILD_VERSION =
+  'export APP_BUILD_VERSION=v$(node -p "require(\'./package.json\').version")-${CODEBUILD_BUILD_NUMBER}';
